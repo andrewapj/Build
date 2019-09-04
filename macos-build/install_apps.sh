ccaeea467132445b09ed25eb901008f313a0a9e9
Andrew Johnson <andrew.apj@gmail.com>
2019-09-04 22:28:41 +0100

    Added 'httpie' to the homebrew apps

--- macos-build/install_apps.sh
+++ macos-build/install_apps.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 brew cask install homebrew/cask-versions/java11 arq atom bitwarden docker google-backup-and-sync google-chrome iterm2 jetbrains-toolbox keka postman vlc
-brew install git gradle maven node wget youtube-dl
+brew install git gradle httpie maven node wget youtube-dl
 cp .bash_profile ~/
 npm install -g typescript
 npm install -g @angular/cli
