c7134b6979860252351f5cc5476dcca3068a71d9
Andrew Johnson <andrew.apj@gmail.com>
2019-11-07 13:54:56 +0000

    Updated MacOS build to configure Zsh

---
+++ macos-build/.zshenv
@@ -0,0 +1,5 @@
+# Set environment variables
+
+export JAVA_HOME=`/usr/libexec/java_home`
+
+alias ll="ls -l"
--- macos-build/install_apps.sh
+++ macos-build/install_apps.sh
@@ -2,5 +2,6 @@
 brew cask install homebrew/cask-versions/java11 arq atom bitwarden docker google-backup-and-sync google-chrome iterm2 jetbrains-toolbox keka postman vlc
 brew install git gradle httpie maven node wget youtube-dl
 cp .bash_profile ~/
+cp .zshenv ~/
 npm install -g typescript
 npm install -g @angular/cli
