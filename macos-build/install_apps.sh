131e5930972b8da831f185068a5bc75371a8877f
Andrew Johnson <andrew.apj@gmail.com>
2020-03-26 23:37:28 +0000

    - Added nmap to install script

--- macos-build/install_apps.sh
+++ macos-build/install_apps.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 brew cask install homebrew/cask-versions/java11 arq atom bitwarden docker google-backup-and-sync google-chrome iterm2 jetbrains-toolbox keka postman vlc
-brew install git gradle httpie maven node wget youtube-dl
+brew install git gradle httpie maven nmap node wget youtube-dl
 cp .bash_profile ~/
 cp .zshenv ~/
