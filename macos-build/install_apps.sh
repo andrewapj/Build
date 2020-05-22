6bd5933d15e5a1f39a30bf562f8601e99da98240
Andrew Johnson <andrew.apj@gmail.com>
2020-05-22 23:23:42 +0100

    - Replaced Atom with Visual Studio Code

--- macos-build/install_apps.sh
+++ macos-build/install_apps.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-brew cask install homebrew/cask-versions/java11 arq atom bitwarden docker google-backup-and-sync google-chrome iterm2 jetbrains-toolbox keka postman vlc
+brew cask install homebrew/cask-versions/java11 arq bitwarden code docker google-backup-and-sync google-chrome iterm2 jetbrains-toolbox keka postman vlc
 brew install git gradle httpie maven nmap node wget youtube-dl
 cp .bash_profile ~/
 cp .zshenv ~/
