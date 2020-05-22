7d59ad35e4d28222d08064b55ae2e7729ef31e90
Andrew Johnson <andrew.apj@gmail.com>
2020-05-22 23:27:00 +0100

    - Fixed Visual Studio Code dependency

--- macos-build/install_apps.sh
+++ macos-build/install_apps.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-brew cask install homebrew/cask-versions/java11 arq bitwarden code docker google-backup-and-sync google-chrome iterm2 jetbrains-toolbox keka postman vlc
+brew cask install homebrew/cask-versions/java11 arq bitwarden docker google-backup-and-sync google-chrome iterm2 jetbrains-toolbox keka postman visual-studio-code vlc
 brew install git gradle httpie maven nmap node wget youtube-dl
 cp .bash_profile ~/
 cp .zshenv ~/
