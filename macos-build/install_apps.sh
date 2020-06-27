247309de12ae81102c46826e019837b9bc67c097
Andrew Johnson <andrew.apj@gmail.com>
2020-06-27 17:12:16 +0100

    - Added VSCode plugins for Yaml, Docker and Kubernetes

--- macos-build/install_apps.sh
+++ macos-build/install_apps.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 brew cask install homebrew/cask-versions/java11 arq bitwarden docker google-backup-and-sync google-chrome iterm2 jetbrains-toolbox keka postman visual-studio-code vlc
 brew install git gradle httpie maven nmap node wget youtube-dl
+
+code --install-extension redhat.vscode-yaml
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-kubernetes-tools.vscode-kubernetes-tools
+
 cp .bash_profile ~/
 cp .zshenv ~/
 cp scripts/* /usr/local/bin/
