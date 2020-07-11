408769b49b432ec1844a9297d9d3d2b43f4adc2f
Andrew Johnson <andrew.apj@gmail.com>
2020-07-11 21:21:18 +0100

    - Updated packages in MacOS install script.

--- macos-build/install_apps.sh
+++ macos-build/install_apps.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 brew cask install homebrew/cask-versions/java11 arq bitwarden docker google-backup-and-sync google-chrome iterm2 jetbrains-toolbox keka postman visual-studio-code vlc
-brew install git gradle httpie maven nmap node wget youtube-dl
+brew install git gradle maven node wget youtube-dl
 
 code --install-extension redhat.vscode-yaml
 code --install-extension ms-azuretools.vscode-docker
