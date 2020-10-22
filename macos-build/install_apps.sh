16fc65ee95d275406c9ede7a6f9dc1e201eb539c
Andrew Johnson <andrew.apj@gmail.com>
2020-10-22 21:55:53 +0100

    - Removed VS Code plugins

--- macos-build/install_apps.sh
+++ macos-build/install_apps.sh
@@ -2,10 +2,6 @@
 brew cask install homebrew/cask-versions/java11 arq bitwarden docker google-backup-and-sync google-chrome iterm2 jetbrains-toolbox keka postman visual-studio-code vlc
 brew install git gradle maven node wget youtube-dl
 
-code --install-extension redhat.vscode-yaml
-code --install-extension ms-azuretools.vscode-docker
-code --install-extension ms-kubernetes-tools.vscode-kubernetes-tools
-
 cp .bash_profile ~/
 cp .zshenv ~/
 cp scripts/* /usr/local/bin/
