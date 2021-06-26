962e349e6ae7495722e5bd184dacbed268929bd7
Andrew Johnson <andrew.apj@gmail.com>
2021-06-26 19:14:59 +0100

    - Updated install script - Updated java version script

--- macos-build/install_apps.sh
+++ macos-build/install_apps.sh
@@ -1,6 +1,16 @@
-#!/usr/bin/env bash
-brew install openjdk@11 git maven node wget youtube-dl
-brew install --cask arq bitwarden docker go google-backup-and-sync google-chrome iterm2 jetbrains-toolbox keka postman visual-studio-code vlc
+#!/usr/bin/env zsh
+
+# Java
+brew install adoptopenjdk adoptopenjdk8 maven
+
+# Go
+brew install go
+
+# Development
+brew install docker git jetbrains-toolbox
+
+# Other
+brew install arq bitwarden google-backup-and-sync google-chrome iterm2 keka postman visual-studio-code vlc youtube-dl
 
 cp .bash_profile ~/
 cp .zshenv ~/
--- macos-build/scripts/java-version.sh
+++ macos-build/scripts/java-version.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
-export JAVA_HOME=$(/usr/libexec/java_home $1)
-
+export JAVA_HOME=$(/usr/libexec/java_home -v $1)
