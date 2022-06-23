ba1e424d96b37e447020d086048c4c10da4df27d
Andrew Johnson <andrew.apj@gmail.com>
2022-06-23 01:28:16 +0100

    Updated casks

--- macos-build/install_apps.sh
+++ macos-build/install_apps.sh
@@ -18,7 +18,7 @@ brew install git jetbrains-toolbox
 brew install --cask docker
 
 # Other
-brew install arq bitwarden google-backup-and-sync google-chrome iterm2 keka postman visual-studio-code vlc youtube-dl
+brew install bitwarden google-drive google-chrome iterm2 keka postman visual-studio-code vlc youtube-dl
 
 cp .zshrc ~/
 cp launchd.conf /etc
