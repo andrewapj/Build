7e538d145a0de27ff5b1158b45ca9cfcf61e689b
Andrew Johnson <andrew.apj@gmail.com>
2026-06-21 13:18:53 +0100

    ammend the brew update script to automatically approve updates

--- macos-build/scripts/update_brew.sh
+++ macos-build/scripts/update_brew.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo "Starting Homebrew maintenance..."
-brew upgrade && brew autoremove && brew cleanup
+brew upgrade -y && brew autoremove && brew cleanup
 echo "Homebrew maintenance complete!"
